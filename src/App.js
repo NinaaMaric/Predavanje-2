dbd17f130c84e4dfbdbb605cafb3147dad12a4d0
Nina <maric.nina@outlook.com'git config --global user.name 'NinaaMaric>
2020-12-28 21:31:11 +0100

    lifecycle methods and events - onClick and onChange + search filter

--- src/App.js
+++ src/App.js
@@ -1,35 +1,116 @@
-import React from 'react'
+import React from "react";
 import "./App.css";
 import { User } from "./User";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
+      userInput: "",
       userInfo: {
-        name: 'John',
-        job: 'Developer'
-      }
-    }
-    fetch('https://rickandmortyapi.com/api/character')
-    .then((response)=>{
-      return response.json()
-    })
-    .then((response)=>{
-      console.log(response.results);
-      let userInfo = response.results[3];
-      this.setState({userInfo})
-    })
-    .catch(function(error){
-      console.log(error);
-    })
-    
+        name: "John",
+        job: "Developer",
+      },
+      allUsers: [],
+      loading: true,
+      counter: 0,
+    };
+    this.setCounter = this.setCounter.bind(this);
+    this.setCounterDecrement = this.setCounterDecrement.bind(this);
   }
+
+  componentDidMount() {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        console.log(response);
+        let allUsers = response.results;
+        this.setState({ allUsers, loading: false });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  // componentDidUpdate(prevState, prevProps){
+  //   console.log(prevProps, prevState);
+  //   if(prevState.name == this.state.name){
+  //     console.log("to je too");
+  //   }
+  // }
+
+  usersToShow() {
+    let counter = this.state.counter;
+    let allUsers = this.state.allUsers.slice(counter, counter + 1, counter - 1);
+    // let userInput = this.state.userInput
+    // let allUsers = this.state.allUsers.filter((el)=> el.name.indexOf(userInput) >= 0)
+    return allUsers.map((el, id) => {
+      return <User key={id} userInfo={el} />;
+    });
+  }
+
+  setCounterDecrement() {
+    let counterr =
+      this.state.allUsers.length > this.state.counter - 1
+        ? this.state.counter - 1
+        : 0;
+    this.setState({ counter: counterr });
+    console.log(counterr);
+  }
+
+  setCounter() {
+    let counter =
+      this.state.allUsers.length > this.state.counter + 1
+        ? this.state.counter + 1
+        : 0;
+    this.setState({ counter });
+    console.log(counter);
+  }
+
+  handleSearchChange = (e) => {
+    console.log("tu sam, mijenjam se", e.target.value);
+    let userInput = e.target.value;
+    this.setState({ userInput });
+  };
+
   render() {
-    console.log(this.state);
+    if (this.state.loading === true) {
+      return (
+        <div className="ui segment" style={{ height: "100vh" }}>
+          <div className="ui active inverted dimmer">
+            <div className="ui text loader">Loading</div>
+          </div>
+          <p></p>
+        </div>
+      );
+    }
     return (
       <div className="wrapper">
-        <User userInfo={this.state.userInfo}></User>
+        <button
+          className="ui left labeled icon button"
+          onClick={this.setCounterDecrement}
+        >
+          <i className="left arrow icon"></i>
+          Prev
+        </button>
+        <button
+          className="ui right labeled icon button"
+          onClick={this.setCounter}
+        >
+          <i className="right arrow icon"></i>
+          Next
+        </button>
+        <div className="ui icon input">
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={this.handleSearchChange}
+          />
+          <i className="search icon"></i>
+        </div>
+        <div>{this.usersToShow()}</div>
       </div>
     );
   }
--- src/User.js
+++ src/User.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 export function User(props) {
+  function newPage(){
+    window.open( props.userInfo.url, '_blank')
+  }
   return (
-    <div className="ui link cards">
+    <div className="ui link cards" onClick={newPage}>
       <div className="card">
-         <div className="image">
+        <div className="image">
           <img src={props.userInfo.image} alt="slika" href="#" />
         </div>
         <div className="content">
-          <div className="header">{props.userInfo.origin.name}</div>
+          <div className="header">{props.userInfo.name}</div>
           <div className="meta">
             <a>Status: {props.userInfo.status}</a>
           </div>
-          <div className="description">
-            Location: {props.userInfo.location.name}
-          </div>
-        </div> 
+          <div className="description">Species: {props.userInfo.species}</div>
+        </div>
       </div>
     </div>
   );
